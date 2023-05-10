75c1e61a69e17b80c6dd02860823e4d8babce836
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-05-10 15:46:58 +0900

    [Feat] 기본적인 레이아웃 구조 잡음
    - 책 데이터를 가지고 가변적으로 탑 쌓기 구현
    - 최상위 레이블과 AsyncImage로 towerCharacter 적용

--- BookfelTower/BookfelTower/Home/HomeView.swift
+++ BookfelTower/BookfelTower/Home/HomeView.swift
@@ -7,20 +7,30 @@
 import SwiftUI
 
 struct HomeView: View {
-    let mockBooks = generateMockBooks()
+    let towerBooks = generateMockBooks()
+    let totalHeight = 65
+    let towerCharacter = "왕귤"
+    let towerCharacterImageURL = "https://cdn-icons-png.flaticon.com/512/135/135620.png"
     
     var body: some View {
-        VStack(spacing:2) {
-            ForEach(mockBooks, id: \.id) { book in
+        VStack(spacing:1) {
+            Text("\(towerCharacter) \(totalHeight)cm")
+            AsyncImage(url: URL(string: towerCharacterImageURL)) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 100, height: 100)
+            ForEach(towerBooks, id: \.id) { book in
                 let dynamicHeight = CGFloat(Int(book.pageNumber) ?? 0) / 3
                 ZStack {
                     RoundedRectangle(cornerRadius: 8)
                         .foregroundColor(.pink)
-                        .frame(width: 180, height: dynamicHeight)
+                        .opacity(Double.random(in: 0.6...0.9))
+                        .frame(width: 200, height: dynamicHeight)
                         .offset(CGSize(width: Int.random(in: -20...20), height: 0))
-                        
                     Text(book.title)
-                }.frame(width: 240).foregroundColor(.white)
+                }.frame(width: 300).foregroundColor(.white)
             }
         }
     }
