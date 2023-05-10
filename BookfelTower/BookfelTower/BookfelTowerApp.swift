7b22567e84ad95dcd64a45ee75b82ef98afe7f65
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-05-10 11:03:31 +0900

    Project Init

---
+++ BookfelTower/BookfelTower/BookfelTowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookfelTowerApp.swift
+//  BookfelTower
+//
+//  Created by Alex Cho on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct BookfelTowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
