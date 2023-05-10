10c57c5ec9820d37e74f888852b5b133655403b0
haram22 <kimhr9889s@gamil.com>
2023-05-10 11:52:30 +0900

    [Chore] BookModel 정의 Book Modeling을 정의 하였습니다.

--- BookfelTower/BookfelTower/Models/BookModel.swift
+++ BookfelTower/BookfelTower/Models/BookModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct BookModel {
+    let title: String
+    let coverUrl: URL
+    let author: String
+    let description: String
+    let publisher: String
+    let isbn: String
+    let pageNumber: String
+    let readingStatus: readStatus?
+    let startDate: Date?
+    let endDate: Date?
+    let rating: Int
+}
+
+enum readStatus{
+    case isDone
+    case isReading
+    case isToRead
+}
