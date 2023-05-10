73fb8343ec0dad4b42919fe666a3b82b915fe650
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-05-10 15:07:03 +0900

    BookModel을 Identifiable 프로토콜에 따르게 함
    BookModel 정의에 Identifiable 프로토콜을 넣고 인스탄스에 id를 부여했했습니다.

--- BookfelTower/BookfelTower/Home/HomeView.swift
+++ BookfelTower/BookfelTower/Home/HomeView.swift
@@ -4,21 +4,27 @@
 //
 //  Created by Alex Cho on 2023/05/10.
 //
-
 import SwiftUI
 
 struct HomeView: View {
     let mockBooks = generateMockBooks()
-
+    
     var body: some View {
-        VStack{
-            ForEach(mockBooks) { book in
-                Text(book.title)
+        VStack(spacing:2) {
+            ForEach(mockBooks, id: \.id) { book in
+                let dynamicHeight = CGFloat(Int(book.pageNumber) ?? 0) / 3
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .foregroundColor(.pink)
+                        .frame(width: 180, height: dynamicHeight)
+                        .offset(CGSize(width: Int.random(in: -20...20), height: 0))
+                        
+                    Text(book.title)
+                }.frame(width: 240).foregroundColor(.white)
             }
         }
     }
 }
-
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
